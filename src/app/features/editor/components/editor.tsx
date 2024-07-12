c2cb5a711d9af388e1258fedc7bd05744cee34fe
Vidit Shah <vidit0210@protonmail.com>
2024-07-12 15:38:48 +0530

    Setting up Canvas

--- src/app/features/editor/components/editor.tsx
+++ src/app/features/editor/components/editor.tsx
@@ -2,18 +2,27 @@
 
 import { useEffect, useRef } from "react";
 import { useEditor } from "../../hooks/use-editor";
-
+import { fabric } from "fabric";
 export const Editor = () => {
   const { init } = useEditor();
-  const workspaceRef = useRef(null);
+  const containerRef = useRef<HTMLDivElement>(null);
   const canvasRef = useRef(null);
   useEffect(() => {
-    init();
-  }, []);
+    const canvas = new fabric.Canvas(canvasRef.current, {
+      controlsAboveOverlay: true,
+      preserveObjectStacking: true,
+    });
+    init({
+      initialContainer: containerRef.current!,
+      initialCanvas: canvas,
+    });
+  }, [init]);
 
   return (
-    <div ref={workspaceRef}>
-      <canvas ref={canvasRef}></canvas>
+    <div className="h-full flex flex-col">
+      <div className="flex-1 h-full bg-muted" ref={containerRef}>
+        <canvas ref={canvasRef}></canvas>
+      </div>
     </div>
   );
 };
--- src/app/features/hooks/use-editor.ts
+++ src/app/features/hooks/use-editor.ts
@@ -1,6 +1,53 @@
+import { useCallback } from "react";
+import { fabric } from "fabric";
+
 export const useEditor = () => {
-  const init = () => {
-    console.log("Editor initialized");
-  };
+  const init = useCallback(
+    ({
+      initialContainer,
+      initialCanvas,
+    }: {
+      initialContainer: HTMLDivElement;
+      initialCanvas: fabric.Canvas;
+    }) => {
+      fabric.Object.prototype.set({
+        cornerColor: "#FFF",
+        cornerStyle: "circle",
+        borderColor: "#3b82f6",
+        borderScaleFactor: 1.5,
+        transparentCorners: false,
+        borderOpacityWhenMoving: 1,
+        cornerStrokeColor: "#3b82f6",
+      });
+      const initialWorkSpace = new fabric.Rect({
+        width: 900,
+        height: 1200,
+        name: "clip",
+        fill: "white",
+        selectable: false,
+        hasControls: false,
+        shadow: new fabric.Shadow({
+          color: "rgb(0,0,0,0.8)",
+          blur: 5,
+        }),
+      });
+
+      initialCanvas.setWidth(initialContainer.offsetWidth);
+      initialCanvas.setHeight(initialContainer.offsetHeight);
+
+      initialCanvas.add(initialWorkSpace);
+      initialCanvas.centerObject(initialWorkSpace);
+      initialCanvas.clipPath = initialWorkSpace;
+
+      const test = new fabric.Rect({
+        width: 100,
+        height: 100,
+        fill: "red",
+      });
+      initialCanvas.add(test);
+      initialCanvas.centerObject(test);
+    },
+    []
+  );
   return { init };
 };
